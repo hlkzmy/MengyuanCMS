6b7a341c267b3d1d534fdc2b06df6d4c7012d34e
hlk <262864308@qq.com>
2014-05-01 22:19:10 +0800

    修改路由配置文件，调整路由

--- web/module/Help/config/module.config.php
+++ web/module/Help/config/module.config.php
@@ -8,24 +8,17 @@ return array(
         	'help' => array(
                 'type'    => 'Segment',
                 'options' => array(
-                    'route'    => '/help[/:controller][/:action][/:id]',
+                    'route'    => '/help[/:controller][/:action]',
                     'constraints' => array(
                         'controller' => '[a-zA-Z][a-zA-Z0-9_-]*',
-                        'action'     => '[a-zA-Z][a-zA-Z0-9_-]*',
-                    	'id'		 => '[0-9]+'
+                        'action'     => '[a-zA-Z][a-zA-Z0-9_-]*'
                     ),
                     'defaults' => array(
                         '__NAMESPACE__' => 'Help\Controller',
                         'controller'    => 'Article',
                         'action'        => 'index',
                     ),
-                	'showSearchList' => array(
-                			'__NAMESPACE__' => 'Help\Controller',
-                			'controller'    => 'Article',
-                			'action'        => 'showSearchList',
-                	),
-                		
-                		
+                	
                 		
                 ),
             ),
@@ -35,7 +28,7 @@ return array(
     
     'controllers' => array(
         'invokables' => array(
-        	'Help\Controller\Index' => 'Help\Controller\IndexController',
+        	'Help\Controller\Index'   => 'Help\Controller\IndexController',
             'Help\Controller\Article' => 'Help\Controller\ArticleController'
         ),
      ),
