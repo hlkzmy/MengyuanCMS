c6205e4347044706be8742e931b7b31a228e38bb
hlk <262864308@qq.com>
2014-05-05 23:45:28 +0800

    添加文章模块的内容，添加一个文章模块组件

--- web/module/Web/src/Web/Controller/AboutController.php
+++ web/module/Web/src/Web/Controller/AboutController.php
@@ -11,14 +11,30 @@ namespace Web\Controller;
 
 use Web\Controller\WebBaseController;
 use Zend\View\Model\ViewModel;
+use Web\Plugin\Article\Column as ArticleColumn;
+
 
 class AboutController extends WebBaseController
 {
     public function indexAction()
     {
     	
+    	$honorViewModel     = new ArticleColumn();
+    	$honorViewModel->setColumnTitle('康润荣誉');
+    	
+    	
+    	$dutyViewModel      = new ArticleColumn();
+    	$dutyViewModel->setColumnTitle('康润社会责任');
+    	
+    	$advantageViewModel = new ArticleColumn();
+    	$advantageViewModel->setColumnTitle('康润优势');
+    	
+    	
 
     	$viewModel = new ViewModel();
+    	$viewModel->addChild( $honorViewModel,'sideArticleColumnViewModel');
+    	$viewModel->addChild( $dutyViewModel,'leftArticleColumnViewModel');
+    	$viewModel->addChild( $advantageViewModel,'rightArticleColumnViewModel');
     	$viewModel->setTemplate("web/common/layout");
     	return $viewModel;
     }
